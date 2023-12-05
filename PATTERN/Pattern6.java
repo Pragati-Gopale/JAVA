6c6a9bed2c2fb8359329a85897889f7925ae92a3
Pragati-Gopale <116192460+Pragati-Gopale@users.noreply.github.com>
2023-12-05 21:55:33 +0530

    Add files via upload

---
+++ PATTERN/Pattern6.java
@@ -0,0 +1,14 @@
+public class Pattern6 {
+    public static void main(String[] args) {
+        int hashcount=3;
+        for(int i=1; i<=6; i++){
+            if(i<=hashcount){
+                System.out.print("#");
+            }
+            else{
+                System.out.print("*");
+            }
+        }
+    }
+    
+}
